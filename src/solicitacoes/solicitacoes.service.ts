ed1184e52c209b7246068e7d011ba2d29775fd40
Arthur Guedes <arthurhenrique96120@gmail.com>
2025-12-05 15:19:48 -0300

    fix: adicionar import Multer no service

--- src/solicitacoes/solicitacoes.service.ts
+++ src/solicitacoes/solicitacoes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, In } from 'typeorm';
+import { Multer } from 'multer';
 import {
   Solicitacao,
   Anexo,
